1fcc670cb39b18ff1efb41cc8e569de2be5288a9
Rafael Noguera <rafanog2155@gmail.com>
2021-05-04 10:35:36 -0300

    12. Expect method
    Use of expect method and eq to build assertions

---
+++ spec/card_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe 'Card' do
+    it 'has a type' do
+        card = Card.new('Ace of Spades') #defines an Ace of Spades card
+        #
+        expect(card.type).to eq('Ace of Spades') #uses the expect method
+    end 
+end
