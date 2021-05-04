072158e7ca8da6e0584f549e80c0e4fd2bfb491e
Rafael Noguera <rafanog2155@gmail.com>
2021-05-04 17:49:22 -0300

    18 let helper
    lazy loading of objects used inside the tests using the let helper

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,5 @@
 class Card
-    attr_reader :rank, :suit
+    attr_accessor :rank, :suit
 
     def initialize(rank, suit)
         @rank = rank
@@ -8,15 +8,15 @@ class Card
 end
 
 RSpec.describe Card do
-    before do
-        @card = Card.new('Ace', 'Spades')
-    end
+    let(:card) { Card.new('Ace', 'Spades') }
     
-    it 'has a rank' do
-        expect(@card.rank).to eq('Ace')
+    it 'has a rank and it can change' do
+        expect(card.rank).to eq('Ace')
+        card.rank = 'Queen'
+        expect(card.rank).to eq('Queen')
     end
 
     it 'has a suit' do
-        expect(@card.suit).to eq('Spades')
+        expect(card.suit).to eq('Spades')
     end
 end
