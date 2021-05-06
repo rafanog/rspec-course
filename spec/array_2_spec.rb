66f22db4cb22a899e7aad08fe72b6d4582af9ed2
Rafael Noguera <rafanog2155@gmail.com>
2021-05-06 12:40:15 -0300

    assignation on explicit subject
    completion of explicit subject assignation

---
+++ spec/array_2_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe Array do
+    subject(:sally) do
+        [1, 2]
+    end
+
+    it 'has length of 2' do
+        expect(sally.length).to eq(2)
+        sally.pop
+        expect(sally.length).to eq(1)
+    end
+
+    it 'has the original length' do
+        expect(sally.length).to eq(2)
+    end
+end
--- spec/explicit_subject_spec.rb
+++ spec/explicit_subject_spec.rb
@@ -1,10 +1,20 @@
 RSpec.describe Hash do
-    subject do
+    subject(:bob) do
         { a: 1, b: 2}
     end
 
+    let(:bob) { { a: 1, b: 2} }
+
     it 'has two key-value pairs' do
         expect(subject.length).to eq(2)
+        expect(bob.length).to eq(2)
+    end
+
+    describe 'nested example' do
+        it 'has to key-value pairs' do
+            expect(subject.length).to eq(2)
+            expect(bob.length).to eq(2)
+        end
     end
 
 end
