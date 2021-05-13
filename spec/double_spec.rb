83ab6f0b50cab1f921853308fa660e30ea630204
Rafael Noguera <rafanog2155@gmail.com>
2021-05-13 14:05:47 -0300

    double_spec
    create strict and doubles that can answer to validations

---
+++ spec/double_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe 'a random double' do
+    it 'only allows defined methods to be invoked' do
+        # stuntman = double('Mr Danger', fall_off_ladder: 'Ouch', light_on_fire: true) #strict double
+        # expect(stuntman.fall_off_ladder).to eq('Ouch')
+        # expect(stuntman.light_on_fire).to eq(True)
+        stuntman = double('Mr. Danger')
+
+        # allow(stuntman).to receive(:fall_off_ladder).and_return('Ouch')
+        # expect(stuntman.fall_off_ladder).to eq('Ouch')
+
+        allow(stuntman).to receive_messages(fall_off_ladder: 'Ouch', light_on_fire: true)
+        expect(stuntman.fall_off_ladder).to eq('Ouch')
+        expect(stuntman.light_on_fire).to eq(true)
+    end
+end
